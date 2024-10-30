90588faa8012ab82906d5ba9e4efbd8b7dad9754
YerassylOM <erasharapov01@gmail.com>
2024-10-30 10:53:36 +0500

    first commit

---
+++ question.js
@@ -0,0 +1,57 @@
+const questions = [
+    {
+        number:1,
+        question: "Who is the champion of champions league 2024?",
+        answer: "A. Real Madrid",
+        options: [
+            "A. Real Madrid",
+            "B. Barcelona",
+            "C. Atletico Madrid",
+            "A. Manchester City"
+        ]
+    },
+    {
+        number:2,
+        question: "Who is The best football player of the world?",
+        answer: "C. Cristiano Ronaldo",
+        options: [
+            "A. Lionel Messi",
+            "B. Arda Guller",
+            "C. Cristiano Ronaldo",
+            "D. Lamine Yamal"
+        ]
+    },
+    {
+        number:3,
+        question: "Who is the best prof young player in the world?",
+        answer: "D. Lamine Yamal",
+        options: [
+            "A. Lionel Messi",
+            "B. Arda Guller",
+            "C. Cristiano Ronaldo",
+            "D. Lamine Yamal"
+        ]
+    },
+    {
+        number:4,
+        question: "What is the best car in the whole world?",
+        answer: "A. MERCEDES CLS63s",
+        options: [
+            "A. MERCEDES CLS63s",
+            "B. BMW M5 F90",
+            "C. AUDI RS8",
+            "D. TAYOTA CAMRY 80"
+        ]
+    },
+    {
+        number:5,
+        question: "What is the best University in Kaskelen",
+        answer: "D. SDU University",
+        options: [
+            "A. SDU",
+            "B. Suleyman Dimeriel University",
+            "D. Samyi Druzhnyi Univer",
+            "D. SDU University"
+        ]
+    }
+]
